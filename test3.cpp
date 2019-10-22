0989b11881427f0c5cf98eabb5aac9b255724620
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-22 16:23:59 +0200

    another cpp file

---
+++ test3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	// calculation
+
+	int firstNumber, SecondNumber;
+	int sum;
+
+	cout << "Enter firstNumber: ";
+	cin >> firstNumber;
+
+	cout << "Enter SecondNumber: ";
+	cint >> SecondNumber;
+
+	sum = firstNumber + SecondNumber 
+	cout<< "Total numbers: "<< sum << endl;
+
+	return 0;
+}
